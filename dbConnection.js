936937167d7063663f202f929498961cd85a5336
enterpriseyes <39146006+enterpriseyes@users.noreply.github.com>
2018-12-23 21:14:52 +0530

    cottecting the dev db

--- dbConnection.js
+++ dbConnection.js
@@ -1,20 +1,19 @@
 const mysql = require('mysql');
 // DB connection configurations test
-// const connection  = mysql.createPool({
-//     connectionLimit : 100,
-//     host: 'yesenterprise.co.in',
-//     user: 'yesenx84_dev_usr',
-//     password: 'Dev@2018',
-//     database: 'yesenx84_cloud_comparison'
-//   });
-//const connection = mysql.createConnection(mysql_pool);
+const connection  = mysql.createPool({
+    connectionLimit : 100,
+    host: 'yesenterprise.co.in',
+    user: 'yesenx84_dev_usr',
+    password: 'Dev@2018',
+    database: 'yesenx84_cloud_compare_dev'
+  });
 
 //DB connection configurations local 
-const connection=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'Test@123',
-    database:'cloud_comparision'
-});
-//connection.connect();
+// const connection=mysql.createConnection({
+//     host:'localhost',
+//     user:'root',
+//     password:'Test@123',
+//     database:'cloud_comparision'
+// });
+
 module.exports = connection;
